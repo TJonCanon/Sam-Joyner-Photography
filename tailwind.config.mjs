f50fe839187604a5adfcdf5d9a3fd5e7e9a1ddb8
TJonCanon <taylor.joyner93@me.com>
2024-02-20 20:28:51 -0600

    Last commit of the night

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				'widescreen': { 'raw': '(min-aspect-ratio: 3/2' },
+				'tallscreen': { 'raw': '(min-aspect-ratio: 13/20'},
+			},
+			keyframs: {
+				'open-menu': {
+					'0%' : { transform: 'scaleY(0)'},
+					'80%' : { transform: 'scaleY(1.2)'},
+					'100%' : { transform: 'scaleY(1)'}
+				}
+			},
+			animation: {
+				'open-menu': 'open-menu 0.5s ease-in-out forwards'
+			}
+		},
 	},
 	plugins: [],
 }
